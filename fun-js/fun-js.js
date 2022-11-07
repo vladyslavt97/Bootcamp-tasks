3d3a7e2c9fd4db4d07354e594a1fb77621f51bd4
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-11-07 13:43:38 +0100

    fun-js

---
+++ fun-js/fun-js.js
@@ -0,0 +1,20 @@
+const x = 876;
+const doubleX;
+
+function timesTwo(number){
+    return number * 2;
+}
+
+const doubleX = timesTwo(x);
+
+
+const numbers = [x, doubleX];
+
+for (let i=0; i<numbers; i=0){
+    console.log(i);
+}
+
+const numbers = {
+    y: doubleX
+};
+
