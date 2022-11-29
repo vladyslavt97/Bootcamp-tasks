1940d560da85dadd93febcc7ebc86311c92382df
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-11-29 16:28:05 +0100

    appendFile

--- week-5/http/index.js
+++ week-5/http/index.js
@@ -1,32 +1,47 @@
 //part 1
 const http = require("http");
 const PORT = 8080;
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
     request.on("error", (err) => console.log("err in request: ", err));
     response.on("error", (err) => console.log("err in response: ", err));
-    if (request.method === "GET" || request.method === "HEAD") {
-        if (request.method === "HEAD") {
-            response.setHeader("content-type", "text/html");
-            response.statusCode = 200;
-            response.write("<h3>write</h3>");
-            response.end(`<h1>End Method</h1>`);
-        } else if (request.url === "/mint") {
-            response.setHeader("content-type", "text/html");
-            response.statusCode = 200;
+    if(request.url === '/secretpage'){
+        console.log('secret page');
+        response.end();
+    }
+    if (request.url === '/'){
 
-            response.end(`<h1>Hello from <b>/Mint</b>!</h1>`);
+        if (request.method === "GET" || request.method === "HEAD") {
+            response.setHeader("content-type", "text/html");
+            response.statusCode = 200;
+            if (request.method === "HEAD") {
+                response.end(`<h1>End Method</h1>`);
+            } else if (request.method === "GET") {
+    
+                response.setHeader("content-type", "text/html");
+                response.statusCode = 200;
+    
+                response.end(`<!doctype html>
+                                <html>
+                                <title>Hello World!</title>
+                                <p>Hello World!</p>
+                                </html>`);
+            }
+        } else if (request.method === "POST") {
+            console.log("POST request body");
+            response.setHeader("Location", "/secretpage");
+            response.statusCode = 302;
+            response.end();
+        } else {
+            response.statusCode = 405;
+            response.end();
         }
-    } else if (request.method === "POST") {
-        console.log("POST request");
-        let body = '';
-        request.on('data', function(chunk) {
-            body += chunk;
-        }).on('end', function() {
-            console.log(body); //logs the entire request body
+        const data = `${request.method} \n ${request.url} \n ${Date}`;
+        fs.appendFile("requests.txt", data, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
         });
-    } else if (request.method !== "POST" || request.method !== "GET" || request.method !== "HEAD") {
-        response.statusCode = 405;
     }
 });
 
