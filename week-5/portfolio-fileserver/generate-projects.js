e8f46b39cfd0c3eaf2c8ef31546f438448ecee3d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-11-30 15:12:32 +0100

    My Projects - portfolio

--- week-5/portfolio-fileserver/generate-projects.js
+++ week-5/portfolio-fileserver/generate-projects.js
@@ -6,15 +6,25 @@ const path = require('path');
 // ]
 // OR more advanced version: use fs.readDirSync for example
 const projects = fs.readdirSync(path.join(__dirname, 'projects'));
-
 module.exports = function generateProjects() {   
     let html = '';
     if (projects.length === 0) {
         return "No results";
     } else {
-        for (let i = 0; i < projects.length; i++) {
-            html += "<a>" + projects[i] + "</a>";
+        for (let i = 1; i < projects.length; i++) {
+            html += `<a href="./${projects[i]}"> ${projects[i]} </a> <br>`;
         }
     }
-    return html;
+    return `<html><head><><body><h1> MY PROJECTS </h1>` + html + `<title>HTML Elements Reference</title> <style> 
+        body {
+            background-color: coral;
+            
+        }
+        a{
+            font-size: 45px;
+        }
+        h1{
+            color: red;
+        }
+        </style>`;
 };
