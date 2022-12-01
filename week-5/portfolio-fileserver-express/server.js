41ad22b0deefed353271404528e66529583ba5b6
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-12-01 17:54:39 +0100

    express (cookies)

--- week-5/portfolio-fileserver-express/server.js
+++ week-5/portfolio-fileserver-express/server.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const generateProjects = require('./generate-projects');
+const generateProjects = require("./generate-projects");
+const cookieParser = require('cookie-parser');
 
-app.use((req, res, next) => {
-    console.log('Hi I am executed first with URL: ', req.url);
+// needs to be called first in order to access req.cookies
+app.use(cookieParser());
+
+app.use((req, res, next) => { 
+    console.log('------------------------------');
+    console.log('req.cookies', req.cookies);
+    console.log('req.url', req.url);
+    console.log('req.method', req.method);
     next();
 });
+// check if we are allowed to visit url by checking cookies
+app.use((req, res, next) => {
+    console.log("Mild");
+    console.log('req.cookies', req.cookies);
 
+    if(req.cookies.accepted !== 'on' && req.url !== '/cookies'){
+        console.log("NO cookies");
+        res.redirect('/cookies');
+    } else if(req.cookies.accepted === 'on'){
+        console.log("With cookies");
+        if (req.url.startsWith('/cookies')){
+            console.log('should get redirected to home');
+            res.redirect('/home');
+        }else {
+            next();
+        }
+        
+    }else{
+        next();
+    }
+});
+
+// middleware to serve static files from a specific folder
 const staticMiddleware = express.static(path.join(__dirname, 'projects'));
 app.use(staticMiddleware);
 
@@ -16,29 +45,30 @@ app.get('/', (req, res) => {
     res.send(finalHtml);
 });
 
+// middleware to encoding the body of a POST request
 const urlEncodedMiddleware = express.urlencoded({ extended: false });
 app.use(urlEncodedMiddleware);
 
-app.post('/hello-world', (req, res) => {
-    console.log('Body of request: ', req.body);
-    const cohort = req.body.cohort;
-    res.send(`Cohort you send was ${cohort}`);
+app.get('/cookies', (req, res)=>{
+    console.log('cookies get');
+    res.sendFile(path.join(__dirname, '/cookies', 'index.html'));
 });
 
 app.get('/home', (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <title>HOME!</title>
-            </head>
-            <body>
-                <p>HOME! from expres</p>
-            </body>
-        </html>`
-    );
-
+    const finalHtml = generateProjects();
+    res.send(finalHtml);
 });
 
-app.listen(3000, () => {
-    console.log('express server is running on localhost:3000...');
+app.post('/cookies', (req, res) => {
+    console.log('req.body', req.body);
+    if(req.body.someSelect === 'accept' && req.body.cohort === 'on'){// if cookie was accepted
+        res.cookie('accepted', 'on');
+        res.redirect('/home/'); // redirect to the initial page
+    } else {// else
+        res.redirect('/cookies/'); // redirect to /cookies
+    }
+});
+
+app.listen(8080, () => {
+    console.log('express server is running on localhost:8080...');
 });
