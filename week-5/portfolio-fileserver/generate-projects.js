d64b4efc3cf279455867ba675deadd5ceef2be51
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-12-01 09:55:28 +0100

    portfolio (with favicon to be solved)

--- week-5/portfolio-fileserver/generate-projects.js
+++ week-5/portfolio-fileserver/generate-projects.js
@@ -12,16 +12,18 @@ module.exports = function generateProjects() {
             html += `<a href="./${projects[i]}"> ${projects[i]} </a> <br>`;
         }
     }
-    return `<h1> MY PROJECTS </h1>` + html + `<title>Portfolio</title> <style> 
+    return `<h1> MY PROJECTS </h1>` + html + `<title>Portfolio</title> <link rel="icon" href="/favicon.ico" type="image/x-icon"> <style> 
         body {
-            background-color: coral;
-            
+            background: rgb(97,159,150);
+            background: linear-gradient(-90deg, rgba(97,159,150,1) 0%, rgba(115,115,115,1) 100%);
         }
         a{
             font-size: 45px;
         }
         h1{
-            color: red;
+            display: flex;
+            justify-content: center;
+            color: white;
         }
         </style>`;
     // return `<!DOCTYPE html>
