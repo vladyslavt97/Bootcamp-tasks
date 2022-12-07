27c400c2cc4616627a1a5cc002ed0859fa59ab36
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2022-12-07 18:03:15 +0100

    fun with fs

--- week-6/fun-with-fs/index.js
+++ week-6/fun-with-fs/index.js
@@ -5,28 +5,45 @@ readdir = promisify(readdir);
 stat = promisify(stat);
 
 const chalk = require('chalk');
-const fs = require('fs');
 const path = require('path');
 
 function logSizes(fullPath){
-    const files = fs.readdirSync(fullPath, { withFileTypes: true});
-    // console.log('my flies: ', files);
-    for (const entity of files) {
-        if (entity.isFile()){
-            const filePath = fullPath + '/' + entity.name;
-            fs.stat(filePath, (err, stats) => {
-                if (err) {
-                    console.log("Error in stat");
+    
+    const resolvedPromises = readdir(fullPath, {withFileTypes: true})
+        .then((files) => {
+            const promises = [];
+            for (const entity of files) {
+                // console.log('entity', entity);
+                const newPath = fullPath + '/' + entity.name;
+                if (entity.isDirectory()){
+                    promises.push(logSizes(newPath));
+                } else {
+                    // const filePath = fullPath + '/' + entity.name;
+                    promises.push(stat(newPath)
+                        .then((stats) => {
+                            console.log(chalk.bold.red('path: '),
+                                chalk.italic.bgWhite.green(newPath), chalk.blue("File size is "), stats.size);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        }));
                 }
-                console.log(chalk.bold.red('path: '), chalk.italic.bgWhite.green(filePath), chalk.blue("File size is "), stats.size);
-            });
-        } 
-        if (entity.isDirectory()){
-            const newPath = fullPath + '/' + entity.name;
-            // console.log('newpath', newPath);
-            logSizes(newPath);
-        }
-    } 
-}
-logSizes(path.join(__dirname, 'files'));
 
+            }
+            return Promise.all(promises);
+        })
+        .catch((error) => {
+            console.log(error);
+        }); 
+
+    return resolvedPromises;
+}
+
+logSizes(path.join(__dirname, 'files'))
+    .then(() => {
+        console.log('DONE!');
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });
